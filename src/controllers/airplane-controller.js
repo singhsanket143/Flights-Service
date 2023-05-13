5f158358337f192daa1d14419f570481749b83b5
swraj28 <swrajkant28@gmail.com>
2023-05-14 01:35:49 +0530

    Implement Update airplane API

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -83,9 +83,30 @@ async function destroyAirplane(req, res) {
     }
 }
 
+/**
+ * PATCH : /api/v1/airplane
+ * req-body {capacity: 250}
+ */
+
+async function updateAirplane(req,res){
+    try {
+        const airplanes = await AirplaneService.updateAirplane(req.params.id,req.body);
+        SuccessResponse.data = airplanes;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
 module.exports = {
     createAirplane,
     getAirplanes,
     getAirplane,
-    destroyAirplane
+    destroyAirplane,
+    updateAirplane
 }
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -54,10 +54,23 @@ async function destroyAirplane(id) {
         throw new AppError('Cannot fetch data of all the airplanes', StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
+
+async function updateAirplane(id,data){
+    try{
+        const response = await airplaneRepository.update(id,data);
+        return response;
+    }catch(error){
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The airplane you requested is not present', error.statusCode);
+        }
+        throw new AppError('Cannot fetch data of all the airplanes', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
  
 module.exports = {
     createAirplane,
     getAirplanes,
     getAirplane,
-    destroyAirplane
+    destroyAirplane,
+    updateAirplane
 }
