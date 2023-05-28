1ba74b5c4835117a0f71982556e4f9fc83583176
Sanket Singh <singhsanket143@gmail.com>
2023-05-28 13:35:37 +0530

    Added flight seats update api

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -76,9 +76,30 @@ async function getFlight(req, res) {
     }
 }
 
+async function updateSeats(req, res) {
+    try {
+        console.log(req.body);
+        const response = await FlightService.updateSeats({
+            flightId: req.params.id,
+            seats: req.body.seats, 
+            dec: req.body.dec
+        });
+        SuccessResponse.data = response;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
 
 module.exports = {
     createFlight,
     getAllFlights,
-    getFlight
+    getFlight,
+    updateSeats
 }
--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const CrudRepository = require('./crud-repository');
 const { Flight, Airplane, Airport, City } = require('../models');
+const db = require('../models');
 
 
 class FlightRepository extends CrudRepository {
@@ -46,6 +47,17 @@ class FlightRepository extends CrudRepository {
             ]
         });
         return response;
+    }
+
+    async updateRemainingSeats(flightId, seats, dec = true) {
+        await db.sequelize.query(`SELECT * from Flights WHERE Flights.id = ${flightId} FOR UPDATE;`);
+        const flight = await Flight.findByPk(flightId);
+        if(parseInt(dec)) {
+            await flight.decrement('totalSeats', {by: seats});
+        } else {
+            await flight.increment('totalSeats', {by: seats});
+        }
+        return flight;
     }
 }
 
