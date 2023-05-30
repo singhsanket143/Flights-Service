cdf524ae509929293419a2bc7272d2bdbca98fea
Sanket Singh <singhsanket143@gmail.com>
2023-05-30 21:01:05 +0530

    Added transaction to update seats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -51,14 +51,22 @@ class FlightRepository extends CrudRepository {
     }
 
     async updateRemainingSeats(flightId, seats, dec = true) {
-        await db.sequelize.query(addRowLockOnFlights(flightId));
-        const flight = await Flight.findByPk(flightId);
-        if(+dec) {
-            await flight.decrement('totalSeats', {by: seats});
-        } else {
-            await flight.increment('totalSeats', {by: seats});
+        const transaction = await db.sequelize.transaction();
+        try {
+            await db.sequelize.query(addRowLockOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if(+dec) {
+                await flight.decrement('totalSeats', {by: seats}, {transaction: transaction});
+            } else {
+                await flight.increment('totalSeats', {by: seats}, {transaction: transaction});
+            }
+            await transaction.commit();
+            return flight;
+        } catch(error) {
+            await transaction.rollback();
+            throw error;
         }
-        return flight;
+       
     }
 }
 
