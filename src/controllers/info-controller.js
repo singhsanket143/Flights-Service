c3ad9713e81dd78a8902bed655b519c16a8ed89d
Sanket Singh <singhsanket143@gmail.com>
2023-06-03 12:20:46 +0530

    made proxy compliant

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 const info = (req, res) => {
+    console.log("Req received")
     return res.status(StatusCodes.OK).json({
         success: true,
         message: 'API is live',
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
+// app.use('/flightsService/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
