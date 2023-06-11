253e6787773cef7eb05035dfe32c0cf04d81160f
rajugupta0 <raju200115@gmail.com>
2023-06-11 09:34:54 +0530

    Added delete city API

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -24,7 +24,22 @@ async function createCity(req, res) {
                 .json(ErrorResponse);
     }
 }
+async function destroyCity(req, res) {
+    try {
+        const cities = await CityService.destroyCity(req.params.id);
+        SuccessResponse.data = cities;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
 
 module.exports = {
-    createCity
+    createCity,
+    destroyCity
 }
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -20,7 +20,19 @@ async function createCity(data) {
         throw new AppError('Cannot create a new city object', StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
+async function destroyCity(id) {
+    try {
+        const response = await cityRepository.destroy(id);
+        return response;
+    } catch(error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The city you requested to delete is not present', error.statusCode);
+        }
+        throw new AppError('Cannot fetch data of all the cities', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
 
 module.exports = {
-    createCity
+    createCity,
+    destroyCity
 }
